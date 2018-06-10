0410e0593523757c1bceb5ff243ca786e5b56280
Dubois Nicolas <dubois.nicolas@free.fr>
2018-06-10 15:50:04 +0200

    add retry if Gladys is not ready on startup

--- app.js
+++ app.js
@@ -4,20 +4,39 @@ const deviceSeen = require('./lib/deviceSeen.js');
 const getDevices = require('./lib/getDevices.js');
 const shared = require('./lib/shared.js');
 
-scan(deviceSeen);
+//retry every 10 seconds
+const RETRY_IN_SECONDS = 10;
+
+//refresh devices every hour
+const REFRESH_DEVICES_FREQUENCY_IN_SECONDS = 3600;
 
 var services = config.services.split(',');
 
-services.forEach((service) => {
-	getDevices(service)
+function getDevicesWithRetry(service) {
+	return getDevices(service)
 		.then((devices) => {
 			// saving devices by identifier
-			devices.forEach((device) => {
+			devices.forEach(function(device) {
 				shared.devices[device.identifier] = device;
 			});
+
+			return null;
 		})
 		.catch((err) => {
-			console.log(`Error while getting ${service} devices from Gladys`);
+			console.log(`Error while getting ${service} devices from Gladys, retrying in ${RETRY_IN_SECONDS} seconds.`);
 			console.log(err);
+			setTimeout(getDevicesWithRetry, RETRY_IN_SECONDS * 1000, service);
 		});
-});
+}
+
+function getServiceDevicesWithRetry() {
+	services.forEach(function(service) {
+		getDevicesWithRetry(service);
+	});
+
+	return null;
+}
+
+getServiceDevicesWithRetry();
+setInterval(getServiceDevicesWithRetry, REFRESH_DEVICES_FREQUENCY_IN_SECONDS * 1000);
+scan(deviceSeen);
