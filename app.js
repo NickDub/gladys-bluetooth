50bbfab6b493767d38509b6874eb400de9c6ff13
Dubois Nicolas <dubois.nicolas@free.fr>
2018-06-10 19:35:01 +0200

    fix, optim and refactoring for PR

--- app.js
+++ app.js
@@ -1,42 +1,28 @@
+const Promise = require('bluebird');
 const config = require('./config.js');
 const scan = require('./lib/scan.js');
 const deviceSeen = require('./lib/deviceSeen.js');
 const getDevices = require('./lib/getDevices.js');
-const shared = require('./lib/shared.js');
-
-//retry every 10 seconds
-const RETRY_IN_SECONDS = 10;
-
-//refresh devices every hour
-const REFRESH_DEVICES_FREQUENCY_IN_SECONDS = 3600;
 
 var services = config.services.split(',');
 
-function getDevicesWithRetry(service) {
-	return getDevices(service)
-		.then((devices) => {
-			// saving devices by identifier
-			devices.forEach(function(device) {
-				shared.devices[device.identifier] = device;
-			});
+// retry every 10 seconds
+const RETRY_IN_SECONDS = 10;
 
-			return null;
-		})
-		.catch((err) => {
-			console.log(`Error while getting ${service} devices from Gladys, retrying in ${RETRY_IN_SECONDS} seconds.`);
-			console.log(err);
-			setTimeout(getDevicesWithRetry, RETRY_IN_SECONDS * 1000, service);
-		});
-}
+// refresh devices every hour
+const REFRESH_DEVICES_FREQUENCY_IN_SECONDS = 3600;
 
-function getServiceDevicesWithRetry() {
-	services.forEach(function(service) {
-		getDevicesWithRetry(service);
+function getDevicesWithRetry() {
+	return Promise.map(services, function(service) {
+		return getDevices(service);
+	})
+	.catch((err) => {
+		console.log(`Error while getting devices from Gladys, retrying in ${RETRY_IN_SECONDS} seconds.`);
+		console.log(err);
+		setTimeout(getDevicesWithRetry, RETRY_IN_SECONDS * 1000);
 	});
-
-	return null;
 }
 
-getServiceDevicesWithRetry();
-setInterval(getServiceDevicesWithRetry, REFRESH_DEVICES_FREQUENCY_IN_SECONDS * 1000);
+getDevicesWithRetry();
+setInterval(getDevicesWithRetry, REFRESH_DEVICES_FREQUENCY_IN_SECONDS * 1000);
 scan(deviceSeen);
